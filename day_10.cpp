0abfde8a1120998aa80db137ebf3ddfceedb174d
bschreiber <birgit.schreiber@voxtronic.com>
2023-06-30 11:00:58 +0200

    Implement challenge day 10 - part 2

--- day_10.cpp
+++ day_10.cpp
@@ -25,23 +25,46 @@ int main() {
 
     int clock = 0;
     int x_register = 1;
-    int sum = 0;
+    int score_part_1 = 0;
     std::vector<int> important_ticks = {20, 60, 100, 140, 180, 220};
 
     for (const auto& command : commands) {
         clock++;
         if (std::find(important_ticks.begin(), important_ticks.end(), clock) != important_ticks.end()) {
 //            std::cout << ">> tick: " << clock << " >> x: " << x_register << " >> product: " << clock*x_register << std::endl;
-            sum += (clock*x_register);
+            score_part_1 += (clock*x_register);
         }
         if (std::regex_search(command, matches, pattern)) {
             x_register += std::stoi(matches[1]);
         }
     }
 
+
     std::cout << "\n*** Results ***" << std::endl;
-    std::cout << "Day " << challenge_id << " - Part 1: " << sum  << std::endl;
+    std::cout << "Day " << challenge_id << " - Part 1: " << score_part_1 << std::endl;
     std::cout << "Day " << challenge_id << " - Part 2: " << std::endl;
+
+    // reset everything for part two
+    clock = 0;
+    x_register = 1;
+    std::vector<int> line_breaks = {40, 80, 120, 160, 200, 240};
+
+    for (const auto& command : commands) {
+        if (x_register == clock || x_register-1 == clock || x_register+1 == clock) {
+            std::cout << "#";
+        } else {
+            std::cout << ".";
+        }
+        clock++;
+
+        if (std::find(line_breaks.begin(), line_breaks.end(), clock) != line_breaks.end()) {
+            std::cout << std::endl;
+            clock -= 40;
+        }
+        if (std::regex_search(command, matches, pattern)) {
+            x_register += std::stoi(matches[1]);
+        }
+    }
 
     return 0;
 }
